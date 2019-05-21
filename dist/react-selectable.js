b9da9917adf740ea88a74cc139092d12c76dd16f
Mickael <mickael@kerjean.me>
2019-05-22 00:00:16 +1000

    fix (csp): firefox reports CSP violation with weird eval :/

--- dist/react-selectable.js
+++ dist/react-selectable.js
@@ -2288,14 +2288,6 @@ g = function () {
 	return this;
 }();
 
-try {
-	// This works if eval is allowed (see CSP)
-	g = g || Function("return this")() || (1, eval)("this");
-} catch (e) {
-	// This works if the window reference is available
-	if ((typeof window === "undefined" ? "undefined" : _typeof(window)) === "object") g = window;
-}
-
 // g can still be undefined, but nothing to do about it...
 // We return undefined, instead of nothing here, so it's
 // easier to handle this case. if(!global) { ...}
